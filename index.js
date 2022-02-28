2f118bc841a81be8eaea5d84d3a7a5a6eae3499e
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-02-28 16:19:46 -0300

    obter dados da requisição

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+//Dizendo para o express usar o ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  const { titulo, descricao } = req.body;
+
+  res.send(`${titulo} ${descricao}`);
 });
 
 app.listen("8080", () => {
