20fc8a94e30c04613b74d15aa50139fa8f4f6b07
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-02-28 12:21:10 -0300

    Estruturas condicionais

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/:nome/:leng", (req, res) => {
+  res.render("index", {
+    ...req.params,
+    empresa: "dddd",
+    msg: false,
+  });
 });
 
 app.listen("8080", () => {
